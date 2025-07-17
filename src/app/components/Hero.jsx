641f0381a77db75ed8c2e4750c27fc5f9464a948
KavinT06 <itskavin47@gmail.com>
2025-07-17 22:04:46 +0530

    Home page responsive added

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -12,9 +12,9 @@ export default function Hero() {
                         <div className="flex items-center justify-between h-16 lg:h-20">
                             <div className="flex-shrink-0 inline-flex">
                                 <a href="#" title="" className="flex">
-                                    <Image className="w-auto h-10 ml-28 rounded-lg" src={logo} alt="" />
+                                    <Image className="w-auto h-7 xl:h-10 xl:ml-28 rounded-lg" src={logo} alt="" />
                                 </a>
-                                <p className='text-xl font-bold text-black ml-2 mt-2'>Aniga's Attire</p>
+                                <p className='text-xl ml-1.5 mt-0.5 font-bold text-black'>Aniga's Attire</p>
                             </div>
 
                             <button type="button" className="inline-flex p-1 text-black transition-all duration-200 border border-black lg:hidden focus:bg-gray-100 hover:bg-gray-100">
@@ -48,10 +48,10 @@ export default function Hero() {
                     </div>
                 </header>
 
-                <section className="py-10 sm:py-16 lg:py-24">
+                <section className="py-10 sm:py-16 xl:py-24">
                     <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
-                            <div>
+                        <div className="grid items-center grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 lg:gap-16">
+                            <div className='md:ml-2'>
                                 <h1 className="text-4xl font-bold text-black sm:text-6xl lg:text-7xl">
                                     Elevate your wardrobe, with
                                     <div className="relative inline-flex">
@@ -63,20 +63,20 @@ export default function Hero() {
                                 <p className="mt-8 text-base text-black sm:text-xl">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat.</p>
 
                                 <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
-                                    <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 hover:bg-orange-600 focus:bg-orange-600" role="button"> Shop now </a>
+                                    <a href="#" title="" className="ml-10 xl:ml-0 inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 hover:bg-orange-600 focus:bg-orange-600" role="button"> Shop now </a>
 
-                                    <a href="#" title="" className="inline-flex items-center mt-6 text-base font-semibold transition-all duration-200 sm:mt-0 hover:opacity-80">
+                                    {/* <a href="#" title="" className="inline-flex items-center mt-6 text-base font-semibold transition-all duration-200 sm:mt-0 hover:opacity-80">
                                         <svg className="w-10 h-10 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path fill="#F97316" stroke="#F97316" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
                                         Watch video
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
 
                             <div>
-                                <Image className="w-4/6 ml-24" src={heroimg} alt="" />
+                                <Image className="w-3/4 md:w-3/4 xl:w-4/6 ml-5 mt-5 xl:mt-0 xl:ml-24" src={heroimg} alt="" />
                             </div>
                         </div>
                     </div>
