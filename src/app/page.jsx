21f798d6f415c578673faa734d88cca28aba6926
KavinT06 <itskavin47@gmail.com>
2025-07-26 11:41:59 +0530

    removed unused files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Hero from "./components/Hero";
-import Products from "./components/Products";
 
 
 export default function Home() {
   return (
     <>
     <Hero />
-    {/* <Products /> */}
     </>
   );
 }
